40c495ec187fee298317e865b74579d16360d00d
nusakakalan <hkknalaka@gmail.com>
2024-08-14 20:01:52 +0530

    added loading skelton and removing duplicated styles

---
+++ src/components/GameCardContainer.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/react";
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+export default function GameCardContainer({ children }: Props) {
+  return (
+    <Box width="300" borderRadius={10} overflow="hidden">
+      {children}
+    </Box>
+  );
+}
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,9 +1,12 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 export default function GameGrid() {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6];
   return (
     <>
       {error && <Text>{error}</Text>}
@@ -12,8 +15,16 @@ export default function GameGrid() {
         spacing={10}
         padding="10px"
       >
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardContainer>
+              <GameCardSkeleton key={skeleton} />
+            </GameCardContainer>
+          ))}
         {games.map((game) => (
-          <GameCard key={game.id} game={game} />
+          <GameCardContainer>
+            <GameCard key={game.id} game={game} />
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </>
