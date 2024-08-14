0ac1b20e13a042eacc75949cbe324a6a914d9acf
nusakakalan <hkknalaka@gmail.com>
2024-08-14 19:06:32 +0530

    Get optimized images.

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -4,7 +4,7 @@ interface Props {
   score: number;
 }
 export default function CriticScore({ score }: Props) {
-  let color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+  const color = score > 75 ? "green" : score > 60 ? "yellow" : "";
   return (
     <Badge fontSize="14px" paddingX={2} borderRadius="4px" colorScheme={color}>
       {score}
