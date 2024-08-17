1e669aea3628e1dcf09ff63bfda76a9608a97c5b
nusakakalan <hkknalaka@gmail.com>
2024-08-17 11:53:02 +0530

    Display genres

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -2,6 +2,7 @@ import useData from "./UseData";
 export interface Genre {
   id: number;
   name: string;
+  image_background: string;
 }
 const useGenres = () => {
   return useData<Genre>("/genres");
