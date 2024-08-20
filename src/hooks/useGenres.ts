b88ffbe677e73e342df05a97fa69eac5345d183c
nusakakalan <hkknalaka@gmail.com>
2024-08-20 13:14:04 +0530

    ship genre with app.

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,11 +1,13 @@
-import useData from "./UseData";
+// import useData from "./UseData";
+import genres from "../data/genres";
 export interface Genre {
   id: number;
   name: string;
   image_background: string;
 }
 const useGenres = () => {
-  return useData<Genre>("/genres");
+  // return useData<Genre>("/genres");
+  return { data: genres, isLoading: false, error: null };
 };
 
 export default useGenres;
